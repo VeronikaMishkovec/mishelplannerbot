78a6fb5afb9fb6876b90ba2d06471edb368528de
VeronikaMishkovec <veronikakireenko@gmail.com>
2023-11-08 17:00:11 +0300

    init project

---
+++ index.js
@@ -0,0 +1,20 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token =  '6666473299:AAGsU2tgxBS1d4S9D-FNz2_tPpzaVH8neus'
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text
+
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'Welcome to your new todo list bot', {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: 'Show all tasks list'}]
+            ]
+        }
+    })
+  }
+});
