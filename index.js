80bcad55cc4c7cab4842abb996e98556c3f0abfe
VeronikaMishkovec <veronikakireenko@gmail.com>
2023-11-10 12:02:06 +0300

    add webapp url

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const token =  '6666473299:AAGsU2tgxBS1d4S9D-FNz2_tPpzaVH8neus'
+const webAppUrl = 'https://keen-sunburst-dfdd00.netlify.app'
 
 const bot = new TelegramBot(token, {polling: true});
 
@@ -12,7 +13,7 @@ bot.on('message', async (msg) => {
     await bot.sendMessage(chatId, 'Welcome to your new todo list bot', {
         reply_markup: {
             inline_keyboard: [
-                [{text: 'Show all tasks list'}]
+                [{text: 'Show all tasks list'}, {web_app: {url: 'https://keen-sunburst-dfdd00.netlify.app/'}}]
             ]
         }
     })
